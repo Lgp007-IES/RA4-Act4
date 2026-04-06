c5d5f72b943419bc862b2a81c847a94898beaea4
Lgp007-IES <lgueper960@g.educaand.es>
2026-04-06 12:29:16 +0200

    raizCuadrada

--- src/miniCalculadora.java
+++ src/miniCalculadora.java
@@ -2,5 +2,7 @@ public class miniCalculadora {
     public double valorAbsoluto(double num){
         return Math.abs(num);
     }
-
+    public double raizCuadrada (double num){
+        return Math.sqrt(num);
+    }
 }
