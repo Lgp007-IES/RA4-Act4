9a448c459d5feee563bd54a05d73e85176bfbdef
Lgp007-IES <lgueper960@g.educaand.es>
2026-04-09 10:02:41 +0200

    Documentacion valor absoluto

--- src/miniCalculadora.java
+++ src/miniCalculadora.java
@@ -1,3 +1,9 @@
+/**
+ * La clase miniCalculadora proporciona métodos para realizar operaciones
+ * matemáticas básicas utilizando la librería Math de Java.
+ * * @author Luis Guerrero
+ * @version 1.0
+ */
 public class miniCalculadora {
     public double valorAbsoluto(double num){
         return Math.abs(num);
