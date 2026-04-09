19051210f374830bf2531f66cf12ecd5a0aa2d25
Lgp007-IES <lgueper960@g.educaand.es>
2026-04-09 10:07:28 +0200

    Documentacion raiz cuadrada

--- src/miniCalculadora.java
+++ src/miniCalculadora.java
@@ -5,9 +5,19 @@
  * @version 1.0
  */
 public class miniCalculadora {
+    /**
+     * Calcula el valor absoluto de un número decimal.
+     * * @param num El número del cual se desea obtener el valor absoluto.
+     * @return El valor absoluto del número introducido.
+     */
     public double valorAbsoluto(double num){
         return Math.abs(num);
     }
+    /**
+     * Calcula la raíz cuadrada de un número decimal.
+     * * @param num El número del que se desea obtener la raíz cuadrada.
+     * @return La raíz cuadrada del número. Si el número es negativo, devolverá NaN.
+     */
     public double raizCuadrada (double num){
         return Math.sqrt(num);
     }
